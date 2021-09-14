d4f5920b289f40b7bc39e8b65e8895050b98eed3
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-14 15:10:37 +0500

    Added Face Detection

--- main.py
+++ main.py
@@ -9,8 +9,18 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    # converting colors to gray
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
     # Display the frame
     cv.imshow('frame', frame)
+
+    # getting face cordinates
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    # showing face cordiates
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
 
     # Closing the window if q is pressed
     if cv.waitKey(20) & 0xFF == ord('q'):
