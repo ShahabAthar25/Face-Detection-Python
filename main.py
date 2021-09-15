efcae6c8f3fb8a9e2fc448441f145e1ebf623749
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-15 18:25:08 +0500

    Add Rectangle Around Face

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2 as cv
 
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -9,23 +8,24 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    # Display the frame
+
     # converting colors to gray
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-    # Display the frame
-    cv.imshow('frame', frame)
 
     # getting face cordinates
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 
     # showing face cordiates
     for (x, y, w, h) in faces:
-        print(x, y, w, h)
+        cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv.imshow('Web Cam', frame)
 
     # Closing the window if q is pressed
     if cv.waitKey(20) & 0xFF == ord('q'):
         break
 
 # releasing capture
-cv.release()
-cv.destryAllWindows()
+cap.release()
+cv.destroyAllWindows()
